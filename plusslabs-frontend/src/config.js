fbffa18b23c5bd22532da9ed3d7cd1058b7a3096
Shubhamsingh2110 <singhshubham1975@gmail.com>
2025-02-14 13:04:51 +0530

    configfile

--- plusslabs-frontend/src/config.js
+++ plusslabs-frontend/src/config.js
@@ -1,2 +1,6 @@
-export const API_BASE_URL = "https://plusslabs-backend.onrender.com/api/auth/signup";
+// export const API_BASE_URL = "https://plusslabs-backend.onrender.com/api/auth/signup";
+
+export const API_BASE_URL = "https://plusslabs-backend.onrender.com";
+
+
 
